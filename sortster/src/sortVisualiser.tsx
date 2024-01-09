99e5a9aecb52339431eae5f915506fbadf6fa992
Mart510 <martynandrewbillings@gmail.com>
2024-01-09 13:59:05 +0000

    bar slider working as expected, states updated

--- sortster/src/sortVisualiser.tsx
+++ sortster/src/sortVisualiser.tsx
@@ -6,16 +6,20 @@ const SortVisualiser = () => {
 // Animation speed controller
 const ANIMATION_SPEED = 1;
 // set num of bars in the array
-let NUMBER_OF_BARS = 420;
+const NUMBER_OF_BARS = 420;
 // Primary colour of array bars
 const BAR_COLOUR = 'teal-900';
 // colour of compared bars (when animated)
 const COMP_COLOUR = 'red-900';
-// bar width (bars to take up 80% of container in total with the spacing taking up the rest)
-let BAR_WIDTH = 1;
 
+// STATE BLOCK
 // state for array bars, init to empty 
 const [barArr, setBarArr] = useState<number[]>([])
+// state for number of columns slider
+const [colNum, setColNum] = useState(NUMBER_OF_BARS)
+// state for width (bars to take up 80% of container in total with the spacing taking up the rest)
+const [barWidth, setBarWidth] = useState<number>((100 / colNum) * 0.8)
+
 
 // reset bar array
 const resetBarArr = (numOfBars : number) => {
@@ -26,25 +30,28 @@ const resetBarArr = (numOfBars : number) => {
         // for each get a random number and add it to the array
         barArray.push
         // values increased by 10 for better visualisation
-        (randomIntInator(10, numOfBars+10));
+        (randomIntInator(10, numOfBars + 10));
     }
     // set the array as the state
     setBarArr(barArray)
-    NUMBER_OF_BARS = numOfBars // updates the global var
-    BAR_WIDTH = ((100/NUMBER_OF_BARS) * 0.8) // updates the global var
+    setColNum(numOfBars) // updates the global var
 }
 
 // Calculating the max value in the array to use to set bar height
 const maxArrayVal : number = Math.max(...barArr)
 
-// state for number of columns slider
-const [colNum, setColNum] = useState(NUMBER_OF_BARS)
 
-// After rendering reset the array
+
+// Re do the bar array on change
     useEffect(() => {
         // update number of cols when slider changes
         resetBarArr(colNum)
     }, [colNum]) // empty dependency array gets it to run just the once
+
+// Recalculate BAR_WIDTH whenever the component is rendered
+    useEffect(() => {
+        setBarWidth((100 / colNum) * 0.8);
+    }, [colNum])
 
 
     return (
@@ -55,7 +62,7 @@ const [colNum, setColNum] = useState(NUMBER_OF_BARS)
                 <input type="range" min="10" max="1000" value={colNum} className="slider" onChange={(e) => setColNum(parseInt(e.target.value, 10))}/>
             </div>
         {/* column container */}
-            <div className="w-full h-[20rem] justify-between flex items-end">
+            <div className="w-full h-[20rem] justify-between flex items-end box-content">
                 {/* columns generated via map */}
                 {barArr.map((value, idx) => (
                     <div
@@ -67,7 +74,7 @@ const [colNum, setColNum] = useState(NUMBER_OF_BARS)
                     style={{
                         backgroundColor: BAR_COLOUR,
                         height: `${(value / maxArrayVal) * 100}%`,
-                        width: `${BAR_WIDTH}%`
+                        width: `${barWidth}%`
                     }}
                     >
                     &nbsp;
