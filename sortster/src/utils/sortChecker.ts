e2adf1ddfd71d97ea479ce24fa078763614c1d08
Mart510 <martynandrewbillings@gmail.com>
2024-01-18 21:43:30 +0000

    sort checker tested

--- sortster/src/utils/sortChecker.ts
+++ sortster/src/utils/sortChecker.ts
@@ -1,7 +1,8 @@
 // To check if an array has been sorted
 export default function sortChecker(arr: number[]): boolean {
     for (let index = 0; index < arr.length - 1; index++) {
-        if (arr[index] >= arr[index + 1]) {
+        // if it's greater than the current value return false (duplicate values are not greater than)
+        if (arr[index] > arr[index + 1]) {
             return false;
         }
     }
