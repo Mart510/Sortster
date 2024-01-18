0c632edbd8ece3b6825bb711032b7d2f7d7c9480
Mart510 <martynandrewbillings@gmail.com>
2024-01-18 18:40:56 +0000

    intial context set up for column num

--- sortster/src/App.tsx
+++ sortster/src/App.tsx
@@ -1,26 +1,26 @@
 // import block
-import React from 'react'
 import './App.css'
 // component importrs
 import SortVisualiser from './components/sortVisualiser'
 import StartResetButton from './components/startResetButton'
 import SortControls from './components/sortControls'
 
+
 function App() {
 
   return (
     <>
-    <div className='mb-4'>
-      <h1>Sortser</h1>
-      <h3>Sorting Algorithm Visualizer</h3>
-    </div>
-      {/* sort controls */}
-      <SortControls/>
-      {/* <ColumnSetter/> */}
-      {/* // sort visualiser */}
-      <SortVisualiser/>
-      {/* start button */}
-      <StartResetButton/>
+      <div className='mb-4'>
+        <h1>Sortser</h1>
+        <h3>Sorting Algorithm Visualizer</h3>
+      </div>
+        {/* sort controls */}
+        <SortControls/>
+        {/* <ColumnSetter/> */}
+        {/* // sort visualiser */}
+        <SortVisualiser/>
+        {/* start button */}
+        <StartResetButton/>
     </>
   )
 }
